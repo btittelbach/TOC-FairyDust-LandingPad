a5834dd24f659a0148e32f74f87717b1414f3323
Bernhard Tittelbach <bernhard@tittelbach.org>
2018-12-22 17:28:47 +0100

    battery check and auto-off

--- TOCfairydustfire.ino
+++ TOCfairydustfire.ino
@@ -37,9 +37,7 @@
 #define BUTTON_PIN 0 //D3 Button
 
 
-#define PHOTORESISTOR_AIN A0
-#define PHOTORESISTOR_PIN 17
-#define PHOTORESISTOR_USE_ADC
+#define BATTERY_TEST_AIN A0
 
 #define NUM_LEDS (93+27)
 #define BUTTON_DEBOUNCE  500
@@ -67,6 +65,7 @@ AnimationTOCFairyDustFire anim_toc;
 AnimationTOCFairyDustLandingRing anim_landing;
 AnimationRainbowGlitter anim_rainbow;
 AnimationFireRing anim_firering;
+AnimationBatteryIndicator anim_battery_indicator;
 
 std::vector<BaseAnimation*> fairy_dust_list =
   {
@@ -77,26 +76,27 @@ std::vector<BaseAnimation*> fairy_dust_list =
 AutoSwitchAnimationCollection fairy_dust_auto_switch(10*60*1000, fairy_dust_list);
 
 std::vector<BaseAnimation*> animations_list_=
-	{
+  {
     &fairy_dust_auto_switch,
     &anim_landing,
     &anim_toc,
     &anim_firering,
     &anim_confetti,
     &anim_rainbow,
-    &anim_sleep_off
+    &anim_sleep_off,
+    &anim_battery_indicator,
   };
 
-uint8_t animation_current_= 0;
 #define NUM_ANIM animations_list_.size()
+uint8_t animation_current_= NUM_ANIM-1; //start with battery indicator
 
 
 
-// This function sets up the ledsand tells the controller about them
+// This function sets up the leds and tells the controller about them
 void setup() {
 #ifdef USE_PJRC_AUDIO
-	AudioMemory(12);
-	delay(2000);
+  AudioMemory(12);
+  delay(2000);
 #endif
 
   //Serial.begin(115200);
@@ -105,20 +105,19 @@ void setup() {
   //wifi / BLE off
   WiFi.mode(WIFI_OFF);
 
-	FastLED.addLeds<WS2812,WS2812_PIN, GRB>(leds_,NUM_LEDS);
-	pinMode(LED_PIN,OUTPUT);
-	digitalWrite(LED_PIN, LOW);
-	pinMode(BUTTON_PIN, INPUT_PULLUP);
-	pinMode(PHOTORESISTOR_AIN, INPUT);
-	pinMode(PHOTORESISTOR_PIN, INPUT);
+  FastLED.addLeds<WS2812,WS2812_PIN, GRB>(leds_,NUM_LEDS);
+  pinMode(LED_PIN,OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BATTERY_TEST_AIN, INPUT);
 #ifdef USE_PJRC_AUDIO
-	pinMode(MICROPHONE_AIN, INPUT);
+  pinMode(MICROPHONE_AIN, INPUT);
 #endif
 
-	//init animation
-	EEPROMBEGIN
-	load_from_EEPROM();
-	animations_list_[animation_current_]->init();
+  //init animation
+  EEPROMBEGIN
+  load_from_EEPROM();
+  animations_list_[animation_current_]->init();
 }
 
 
@@ -139,22 +138,43 @@ void load_from_EEPROM()
   animation_current_ = EEPROM.read(EEPROM_ADDR_CURANIM) % NUM_ANIM;
 }
 
+void task_check_battery()
+{
+  static millis_t next_check=0;
 
+  //ignore overflow for now, as the only thing that happens that we check all the time until 20s have passed.
+  if (millis() < next_check)
+    return;
+
+  next_check=millis()+1000*20;
+
+  //full charge with 4.2V at about 930 (out of 1024)
+  //empty with 2.85V at about 650 (out of 1024)
+  //battery charge thus ranges from 0 .. 255 to indicate charge
+  uint16_t const batt_empty = 650;
+  uint16_t const batt_full = 930;
+  uint16_t adc_reading = analogRead(BATTERY_TEST_AIN);
+  uint16_t batt_charge_byte = min(0xff,(max(batt_empty,adc_reading)-batt_empty) * 0xff / (batt_full-batt_empty));
+  anim_battery_indicator.setBatteryChargeLevel0to255(batt_charge_byte);
+
+  if (adc_reading < batt_empty/2)
+  {
+    //no battery connected
+    return;
+  }
+
+  if (adc_reading <= batt_empty)
+  {
+    //switch to power saving
+    animation_switch_to_off_if_in_list();
+    return;
+  }
+}
 
 void task_check_lightlevel()
 {
-#ifdef PHOTORESISTOR_USE_ADC
-#ifdef USE_PJRC_AUDIO
-  if (!photoPeak.available())
-    return;
-  light_level = photoPeak.read()*4095;
-#else
-  light_level = analogRead(PHOTORESISTOR_AIN);
-#endif
+  light_level = 0; //TODO
   if (light_level > LIGHT_THRESHOLD)
-#else
-  if (digitalRead(PHOTORESISTOR_PIN) == LOW)
-#endif
   {
     //assume daylight
     if (dark_count_ < LIGHT_DEBOUNCE) {
@@ -206,6 +226,20 @@ void animation_switch_next()
   animation_current_%=NUM_ANIM;
   save_to_EEPROM();
   animations_list_[animation_current_]->init();
+}
+
+void animation_switch_to_off_if_in_list()
+{
+  for (unsigned int c=0; c < NUM_ANIM; c++)
+  {
+    if (animations_list_[c] == &anim_sleep_off)
+    {
+      animation_current_ = c;
+      save_to_EEPROM();
+      animations_list_[animation_current_]->init();
+      break;
+    }
+  }
 }
 
 void task_check_button()
